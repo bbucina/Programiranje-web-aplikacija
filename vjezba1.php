6a0d889de90019be968bbb28dd0e0e514b64b1a7
bbucina <bbucina2@gmail.com>
2026-03-24 23:44:28 +0100

    Add files via upload

---
+++ vjezba1.php
@@ -0,0 +1,10 @@
+<?php
+    $naslov = "Moj prvi PHP dokument";
+    $autor = "Barbara Bucina";
+
+    echo "<h1>$naslov</h1>";
+    echo "<p>Ovu stranicu izradila je <strong>$autor</strong>.</p>";
+    echo "<a href = 'https://www.w3schools.com/php/default.asp' target = '_blank> Posjeti W3SCHOOLS</a>"
+?>
+
+<!-- Naziv datoteke: vjezba1.php -->
